09af16c2ec2bab5ab525be56d2db82b645087f88
tom <tom@stellaqua.com>
2009-03-10 20:28:58 +0900

    keyとvalueがタブ区切りで記述されている場合に対応するようにした。

--- HadoopStreaming/Mapper.php
+++ HadoopStreaming/Mapper.php
@@ -1,13 +1,29 @@
 <?php
 abstract class HadoopStreaming_Mapper
 {
+	private $is_tab_separated;
+
+	public function __construct ( $is_tab_separated = false )
+	{
+		$this->is_tab_separated = $is_tab_separated;
+	}
+	
 	abstract function map ( $key, $value );
 	
 	public function run (  )
 	{
 		while ( !feof(STDIN) ) {
 			$line = trim(fgets(STDIN));
-			$this->map(null, $line);
+			if ( $line !== '' ) {
+				if ( $this->is_tab_separated === true
+					 && strpos($line, "\t") !== false ) {
+					list($key, $value) = split("\t+", $line, 2);
+				} else {
+					$key = '';
+					$value = $line;
+				}
+				$this->map($key, $value);
+			}
 		}
 	}
 	
